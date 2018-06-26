c8f8219018c6471c823ea2a3fe2790c33f3bfbfa
Luke Whyte <lukeawhyte@gmail.com>
2018-06-25 19:23:49 -0500

    Main headless browser work complete, debugging narrative, will need to refactor

--- src/scrapeDeficiencyDetails/index.ts
+++ src/scrapeDeficiencyDetails/index.ts
@@ -1,7 +1,10 @@
 import * as puppeteer from 'puppeteer';
 
 // interfaces
-import { DeficiencyHash } from '../interfaces';
+import { DeficiencyHash, DeficencyPopUpHash } from '../interfaces';
+
+// modules
+import { clickButton, resolveButtonClick } from '../handleClickEvents';
 
 const handleError = (err: any) => {
 	console.error(err);
@@ -17,18 +20,51 @@ const defaultPayload = () => ({
     corrected_at_inspection: null,
     corrected_date: 'None',
     date_correction_verified: 'None',
+    technical_assistance_given: null,
     narrative: 'None',
 });
 
 export const getURL = (id: number) => `https://www.dfps.state.tx.us/Child_Care/Search_Texas_Child_Care/CCLNET/Source/Provider/ppComplianceHistory.aspx?fid=${id}&tab=2`;
 
+export const closeSubscriptionModal = async page => {
+	return await page.click('button.prefix-overlay-close.prefix-overlay-action-later').catch((err) => console.log('No modal'));
+};
+
 export const getDeficencyPage = async (id: number, browser: any) => {
 	const page = await browser.newPage();
 	await page.goto(getURL(id)).catch(handleError);
+	// We need to handle the modal that pops up on page load
+	await closeSubscriptionModal(page);
 	return page;
 };
 
 export const getDeficenciesRow = page => page ? page.$$('#ctl00_contentBase_tabSections_C1 .dxgvDataRow_Glass') : [];
+
+export const getTechnicalAssistanceGiven = async page => {
+	const technical_assistance_given = await page.$eval('span#ctl00_contentBase_popupNarrative_ASPxLabel1', node => {
+		const html = node.innerHTML.trim();
+		return html.slice(html.indexOf(':') + 1);
+	});
+	return { technical_assistance_given };
+};
+
+export const getNarrative = async page => {
+	const narrative = await page.$eval('textarea.dxeMemoEditArea_Glass dxeMemoEditAreaSys', nodes => nodes.map(node => node.innerHTML.trim()));
+	return { narrative };
+};
+
+export const getPopUpHash = async (elementHandle, page, id: number) => {
+	const targetGroup 	= await elementHandle.$$('.dxgv');
+	const target		= targetGroup[7];
+	const isClickSuccessful = await clickButton(page, target, getURL(id));
+	if (isClickSuccessful) {
+		const technicalAssistanceGiven = await getTechnicalAssistanceGiven(page);
+		const narrative = await getNarrative(page);
+		return Object.assign({}, technicalAssistanceGiven, narrative);
+	} else {
+		return handlePopUpError('Click failed!');
+	}
+};
 
 export const getString = (cells, elementHandle, cellsIdx: number) => typeof cells[cellsIdx] === 'string' && cells[cellsIdx].length > 0 ? cells[cellsIdx] : 'None'
 
@@ -82,33 +118,49 @@ export const pluckValues = (cells, elementHandle) => {
 	return result;
 };
 
-export const getIncident = async elementHandle => {
+const handlePopUpError = (err: any) => {
+	console.error(err);
+	return { technical_assistance_given: null, narrative: 'Error plucking!!!' };
+};
+
+export const getIncident = async (page, id: number, elementHandle) => {
 	const cells = await elementHandle.$$eval('.dxgv', nodes => nodes.map(node => node.innerHTML.trim()));
+	const popupContent = await getPopUpHash(elementHandle, page, id).catch(handlePopUpError);
 	return pluckValues(cells, elementHandle);
 };
 
-export const findDeadButton = page => {
-	const deadButtons = page.$$eval('b.dxp-button.dxp-bi.dxp-disabledButton', nodes => nodes.map(node => node.innerHTML.trim()));
-	const deadNextButton = deadButtons.filter(btn => btn.indexOf('<img src="../../App_Themes/Office2003%20Blue/Web/pNextDisabled.png" alt="Next">') !== 0);
+export const findDeadButton = async page => {
+	const deadButtons = await page.$$eval('b.dxp-button.dxp-bi.dxp-disabledButton', nodes => nodes.map(node => node.innerHTML.trim())).catch(() => []);
+	const deadNextButton = deadButtons.filter(btn => btn.indexOf('<img src="../../App_Themes/Office2003%20Blue/Web/pNextDisabled.png" alt="Next">') !== -1);
 	return deadNextButton.length === 1;
 };
 
-export const scrapeRowsFromTable = async (payload: Array<DeficiencyHash>, page) => {
+export const clickNextButton = async (page, id: number) => {
+	const onClickSel = 'ASPx.GVPagerOnClick(\'ctl00_contentBase_tabSections_gridSummary\',\'PBN\');'
+	const fullSel = `a.dxp-button.dxp-bi[onclick="${onClickSel}"]`;
+	return clickButton(page, fullSel, getURL(id));
+};
+
+export const scrapeRowsFromTable = async (payload: Array<DeficiencyHash>, page, id: number) => {
 	const rows = await getDeficenciesRow(page);
-	const incidents = await Promise.all(rows.map(getIncident)).catch(handleError);
+	const incidents: Array<DeficiencyHash> = await Promise.all(rows.map(getIncident).bind(null, page, id)).catch(handleError);
 	payload = payload.concat(incidents);
 
 	const isDeadButton = await findDeadButton(page);
-	// afterwards, click the button and wait to see if another row loads
-	// if row has loaded, restart the process
-	// when button finally fails to return results, return payload
+	if (!isDeadButton) {
+		const isClickSuccessful = await clickNextButton(page, id);
+		if (!isClickSuccessful) return payload;
+		return await scrapeRowsFromTable(payload, page, id).catch(handleError);
+	} else {
+		return payload;
+	}
 };
 
 export default async (id: number) => {
 	const browser 		= await puppeteer.launch();
 	const page 			= await getDeficencyPage(id, browser);
 
-	const payload: Promise<Array<DeficiencyHash>> = await scrapeRowsFromTable([], page).catch(handleError);
+	const payload: Promise<Array<DeficiencyHash>> = await scrapeRowsFromTable([], page, id).catch(handleError);
 
 	return {
 		payload: [
--- src/scrapeDeficiencyDetails/spec.ts
+++ src/scrapeDeficiencyDetails/spec.ts
@@ -5,40 +5,153 @@ import {
 	getDeficencyPage,
 	getDeficenciesRow,
 	getIncident,
+	findDeadButton,
+	clickNextButton,
+	scrapeRowsFromTable,
+	getPopUpHash,
 } from './index';
 
-test('puppeteer pulls the ten deficencies from the page and stores them in a NodeList', async t => {
-	const browser 	= await puppeteer.launch();
-	const page 		= await getDeficencyPage(95732, browser);
-	const rows		= await getDeficenciesRow(page);
+const runTests = async () => {
+	const debugOpts = { headless: false, slowMo: 250 };
+	const browser = await puppeteer.launch(debugOpts);
 
-	let boolResult = rows.length === 10;
-	let boolRxpected = true;
-	t.equal(boolResult, boolRxpected);
+	// test('puppeteer pulls the ten deficencies from the page and stores them in a NodeList', async t => {
+	// 	const page = await getDeficencyPage(95732, browser);
+	// 	const rows = await getDeficenciesRow(page);
 
-	await browser.close();
-	t.end();
-});
+	// 	let boolResult = rows.length === 10;
+	// 	let boolRxpected = true;
+	// 	t.equal(boolResult, boolRxpected);
 
-test('getIncident: should return a DeficiencyHash with scraped values', async t => {
-	const browser 	= await puppeteer.launch();
-	const page 		= await getDeficencyPage(1252288, browser);
-	const rows		= await getDeficenciesRow(page);
+	// 	await page.close();
+	// 	t.end();
+	// });
 
-	let result = await getIncident(rows[0]);
-	let expected = {
-		activity_date: '6/15/2018',
-		activity_id: null,
-		standard_number_description: '748.1003(a) - Child/caregiver ratio-Caregiver may care for 5 children if any require tx svcs, 8 children if not; children under 5 yrs old count as 2 children',
-		activity_type: 'Assessments',
-		standard_risk_level: 'Medium High',
-		corrected_at_inspection: false,
-		corrected_date: '6/18/2018',
-		date_correction_verified: '6/18/2018',
-		narrative: 'None' 
-	};
-	t.deepEqual(result, expected);
+	// test('getIncident: should return a DeficiencyHash with scraped values', async t => {
+	// 	const page = await getDeficencyPage(1252288, browser);
+	// 	const rows = await getDeficenciesRow(page);
 
-	await browser.close();
-	t.end();
-});
+	// 	let result = await getIncident(rows[0]);
+	// 	let expected = {
+	// 		activity_date: '6/15/2018',
+	// 		activity_id: null,
+	// 		standard_number_description: '748.1003(a) - Child/caregiver ratio-Caregiver may care for 5 children if any require tx svcs, 8 children if not; children under 5 yrs old count as 2 children',
+	// 		activity_type: 'Assessments',
+	// 		standard_risk_level: 'Medium High',
+	// 		corrected_at_inspection: false,
+	// 		corrected_date: '6/18/2018',
+	// 		date_correction_verified: '6/18/2018',
+	// 		narrative: 'None' 
+	// 	};
+	// 	t.deepEqual(result, expected);
+
+	// 	await page.close();
+	// 	t.end();
+	// });
+
+	// test('clickNextButton: should return successful selector if button was clicked, or null if not', async t => {
+	// 	const page = await getDeficencyPage(141349, browser);
+		
+	// 	await clickNextButton(page, 141349);
+	// 	const rows = await getDeficenciesRow(page);
+
+	// 	let result = await getIncident(rows[0]);
+	// 	let expected = {
+	// 		activity_date: '7/10/2017',
+	// 		activity_id: null,
+	// 		standard_number_description: '748.1387 - Service Plan Review-Must comply with notification, participation, implementation, and documentation requirements for an initial service plan',
+	// 		activity_type: 'Monitoring Inspections',
+	// 		standard_risk_level: 'Medium Low',
+	// 		corrected_at_inspection: false,
+	// 		corrected_date: '7/24/2017',
+	// 		date_correction_verified: '7/31/2017',
+	// 		narrative: 'None'
+	// 	};
+	// 	t.deepEqual(result, expected);
+
+	// 	await page.close();
+	// 	t.end();
+	// });
+
+	// test('findDeadButton: When we reach the end of pagination, this should stop the process', async t => {
+	// 	const page = await getDeficencyPage(95732, browser);
+	// 	let successfulClicks = 0;
+	// 	let isSuccessfulClick = false;
+
+	// 	let isButtonDead = await findDeadButton(page);
+
+	// 	while (!isButtonDead) {
+	// 		isSuccessfulClick = await clickNextButton(page, 95732);
+	// 		if (!isSuccessfulClick) break;
+	// 		successfulClicks++;
+	// 		console.log(`${successfulClicks} successful clicks`);
+	// 		isButtonDead = await findDeadButton(page);
+	// 	}
+
+	// 	let result = successfulClicks;
+	// 	let expected = 3;
+	// 	t.equal(result, expected);
+
+	// 	await page.close();
+	// 	t.end();
+	// });
+
+	// test('scrapeRowsFromTable: Should grab all possible deficiencies and return array of hashes', async t => {
+	// 	const page = await getDeficencyPage(95732, browser);
+
+	// 	const deficiencies = await scrapeRowsFromTable([], page, 95732);
+
+		
+	// 	let resultFirstDeficency = deficiencies[0]
+	// 	let expectedFirstDeficiency = { 
+	// 		activity_date: '12/16/2017',
+	// 	    activity_id: null,
+	// 	    standard_number_description: '749.1803(e) - Infant requirements-Infant must never be left unsupervised',
+	// 	    activity_type: 'Self Reported Incidents',
+	// 	    standard_risk_level: 'High',
+	// 	    corrected_at_inspection: false,
+	// 	    corrected_date: '1/19/2018',
+	// 	    date_correction_verified: '1/19/2018',
+	// 	    technical_assistance_given: null,
+	// 	    narrative: 'None' 
+	// 	};
+	// 	t.deepEqual(resultFirstDeficency, expectedFirstDeficiency);
+
+	// 	let resultLastDeficency = deficiencies[deficiencies.length - 1];
+	// 	let expectedLastDeficiency = { 
+	// 		activity_date: '7/20/2015',
+	// 	    activity_id: null,
+	// 	    standard_number_description: '749.1957(1) - Other Prohibited Discipline-Any harsh, cruel, unusual, unnecessary, demeaning, or humiliating discipline or punishment',
+	// 	    activity_type: 'Reports',
+	// 	    standard_risk_level: 'High',
+	// 	    corrected_at_inspection: false,
+	// 	    corrected_date: '10/2/2015',
+	// 	    date_correction_verified: '10/2/2015',
+	// 	    technical_assistance_given: null,
+	// 	    narrative: 'None'
+	// 	};
+	// 	t.deepEqual(resultFirstDeficency, expectedFirstDeficiency);
+
+	// 	let resultLen = deficiencies.length;
+	// 	let expectedLen = 37
+	// 	t.equal(resultLen, expectedLen);
+
+	// 	await page.close();
+	// 	t.end();
+	// 	process.exit(0)
+	// });
+
+	test('getPopUpHash: return hash with content from narrative popup', async t => {
+		const page = await getDeficencyPage(95732, browser);
+		const rows = await getDeficenciesRow(page);
+
+		let result = await getPopUpHash(rows[0], page, 95732);
+		let expected = { stuff: 'here' };
+		t.deepEqual(result, expected);
+
+		await browser.close();
+		t.end();
+		process.exit(0)
+	});
+}
+runTests();
